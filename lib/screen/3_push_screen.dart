e5c3fea80c0845b730b568cf5c4e55f377c987f2
hee <songyn111@gmail.com>
2023-06-02 15:48:20 +0900

    push와 go의 차이 예제

---
+++ lib/screen/3_push_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_go_router_example/layout/default_layout.dart';
+import 'package:go_router/go_router.dart';
+
+class PushScreen extends StatelessWidget {
+  const PushScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        body: ListView(
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              //바로 전 route로 이동
+              context.push('/basic');
+            },
+            child: Text('Push Basic')),
+        ElevatedButton(
+            onPressed: () {
+              //제일 상위 route로 이동
+              context.go('/basic');
+            },
+            child: Text('Go Basic'))
+      ],
+    ));
+  }
+}
